e1f1d8c919392d5d1433e8b71b7a9e32e21fd78a
REALFINIT <53615544+REALFINIT@users.noreply.github.com>
2022-11-08 16:03:08 +0330

    themefunc 1

--- scripts.js
+++ scripts.js
@@ -1,2 +1,13 @@
 alert("welcome");
-prompt('what is your name?');
+    let person = prompt("what's your name?");
+    if (person != null) {
+      document.getElementById("name").innerHTML =
+      "Hi " + person + "! wellcome to my site:)";
+    }
+    function themefunc() {
+        if (themefunc != null) {
+          document.getElementById("themeask").innerHTML =
+          "which color do you perfer?";
+        }
+      }
+
